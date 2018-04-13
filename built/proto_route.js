c7ff48611601697d50556be809e272c698ec3745
雪霁 <wang.haiguang@puscene.com>
2018-04-13 11:08:45 +0800

    .

--- built/proto_route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const path = require("path");
-const PROTO_BASE_PATH = path.resolve(__dirname, '../protos');
-const route = function (proto) {
-    return path.resolve(PROTO_BASE_PATH, proto);
-};
-exports.default = route;
